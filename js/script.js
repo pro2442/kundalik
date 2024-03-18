880be81528e603fed299eee8706dbc3e0cd03c44
pro2442 <pro2442geme@gmail.com>
2024-03-18 14:34:07 +0500

    first

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// let doc = document
+
+// fetch("https://api.emaktab.uz/v1/users/1000002293998")
+//   .then(response => response.json()) // Декодируем ответ в формате json
+//   .then(data => console.log(data)); // Выводим ответ в консоль
